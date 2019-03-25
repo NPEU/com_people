d0de94e13e78fe7417b6a0217aee2915a39b853b
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2019-03-25 07:15:02 +0000

    Set basic flag to drastically improve load time

--- site/models/people.php
+++ site/models/people.php
@@ -20,7 +20,7 @@ class PeopleModelPeople extends JModelLegacy
     public function getPeople()
     {
         $base_url = JUri::base();
-        $data     = json_decode(file_get_contents($base_url . 'data/staff?collect=displaygroup&order=lastname'), true);
+        $data     = json_decode(file_get_contents($base_url . 'data/staff?collect=displaygroup&order=lastname&basic=1'), true);
 
         // Exclude 'pseudo' members from list.
         // 608 = Maggie - not sure if I can just disable her account at this stage.
